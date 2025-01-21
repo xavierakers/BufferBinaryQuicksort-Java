47f29d311dd23fae3ee318f74e11aa5eda53628f
Xavier Akers <x-akers@hotmail.com>
2025-01-21 12:43:20 -0500

    Added BufferPoolInterface.java javadoc.

--- src/main/java/com/xakers/buffer/BufferPoolInterface.java
+++ src/main/java/com/xakers/buffer/BufferPoolInterface.java
@@ -1,5 +1,44 @@
 package main.java.com.xakers.buffer;
 
-public interface Buffer {
-    
+import java.io.IOException;
+
+/**
+ * Interface for BufferPool operations
+ */
+public interface BufferPoolInterface {
+
+    /**
+     * Inserts data into the buffer pool.
+     *
+     * @param data        The byte array containing the data to be inserted.
+     * @param dataSize    The size of the data to be inserted, in bytes.
+     * @param absolutePos The absolute position in the file where the data should be inserted.
+     * @throws Exception If an error occurs during the insertion process.
+     */
+    void setBytes(byte[] data, int dataSize, int absolutePos) throws Exception;
+
+    /**
+     * Retrieves data from the buffer pool.
+     *
+     * @param data        The byte array to store the retrieved data.
+     * @param dataSize    The size of the data to retrieve, in bytes.
+     * @param absolutePos The absolute position in the file from which the data should be retrieved.
+     * @throws Exception If an error occurs during the retrieval process.
+     */
+    void getBytes(byte[] data, int dataSize, int absolutePos) throws Exception;
+
+    /**
+     * Flushes all dirty buffers to the underlying storage and clears the buffer pool.
+     *
+     * @throws IOException If an error occurs while writing data to the storage.
+     */
+    void flush() throws IOException;
+
+    /**
+     * Gets the total size of the underlying data file.
+     *
+     * @return The size of the data file, in bytes
+     */
+    int getDataFileSize() throws IOException;
+
 }
